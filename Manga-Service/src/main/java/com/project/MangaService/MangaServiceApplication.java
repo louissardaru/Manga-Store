6feae58e430c628f84d01a971927893fb8e6fd01
AzureAD\LouisSardaru <louis.sardaru@finlence.com>
2023-10-15 10:47:17 +0300

    first commit

---
+++ Manga-Service/src/main/java/com/project/MangaService/MangaServiceApplication.java
@@ -0,0 +1,13 @@
+package com.project.MangaService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MangaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MangaServiceApplication.class, args);
+	}
+
+}
---
+++ Manga-Service/src/test/java/com/project/MangaService/MangaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.MangaService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MangaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
